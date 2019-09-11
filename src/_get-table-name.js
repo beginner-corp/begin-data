d14d8f4e645d40807073a82923bfbdf985dfb10c
Brian LeRoux <b@brian.io>
2019-09-11 15:35:46 -0700

    possible fix for cache bug

--- src/_get-table-name.js
+++ src/_get-table-name.js
@@ -1,12 +1,12 @@
 let getTableNameForSandbox = require('./_get-table-name-sandbox')
-let getTableName = require('./_get-table-name-ssm')
+let getTableNameForArc6 = require('./_get-table-name-ssm')
 let olds = require('./_get-table-name-old')
 
 module.exports = function _getTableName(callback) {
 
   let override = process.env.hasOwnProperty('BEGIN_DATA_TABLE_NAME')
   let sandbox = process.env.NODE_ENV === 'testing' || process.env.ARC_LOCAL
-  let arc5 = !!process.env.DEPRECATED
+  let arc5 = process.env.DEPRECATED || !process.env.ARC_CLOUDFORMATION
   let arc6 = !!process.env.ARC_CLOUDFORMATION
 
   if (override) {
@@ -22,7 +22,7 @@ module.exports = function _getTableName(callback) {
   }
   else if (arc6) {
     // read SSM param for data table
-    getTableName(callback)
+    getTableNameForArc6(callback)
   }
   else {
     throw ReferenceError('begin/data could not find the data table')
