c1f86527bcc2ed618520e4c84e6153c7457d4523
Ryan Block <rblock@gmail.com>
2020-02-05 18:51:56 -0800

    Removes unnecessary aws.config.update calls / env var clobbering

--- src/_get-db.js
+++ src/_get-db.js
@@ -11,15 +11,6 @@ let DB = aws.DynamoDB
 let port = process.env.ARC_TABLES_PORT || 5000
 let endpoint = new aws.Endpoint(`http://localhost:${port}`)
 let testing = process.env.NODE_ENV === 'testing'
+let region = 'us-west-2'
 
-if (testing) {
-  if (!process.env.AWS_SECRET_ACCESS_KEY)
-    process.env.AWS_SECRET_ACCESS_KEY = 'xxx'
-  if (!process.env.AWS_ACCESS_KEY)
-    process.env.AWS_ACCESS_KEY_ID = 'xxx'
-  aws.config.update({
-    region: 'us-west-1'
-  })
-}
-
-module.exports = testing? new DB({endpoint}) : new DB
+module.exports = testing? new DB({endpoint, region}) : new DB
