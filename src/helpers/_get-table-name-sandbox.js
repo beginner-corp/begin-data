48b31016e9b9791e137bffe501bc9f4699cd2399
Brian LeRoux <b@brian.io>
2020-11-11 20:03:54 -0800

    fix lint

--- src/helpers/_get-table-name-sandbox.js
+++ src/helpers/_get-table-name-sandbox.js
@@ -4,7 +4,7 @@ let { readArc } = require('@architect/parser')
 /**
  * Arc 6+ paths where project manifest needs to be read from root
  */
-module.exports = function getTableName() {
+module.exports = function getTableName () {
   // Try cwd first (usually testing)
   let result = readArc()
   // Try project root next (usually from within a Sandbox Lambda)
@@ -13,7 +13,7 @@ module.exports = function getTableName() {
   if (!result.filepath) throw ReferenceError('Architect project manifest not found')
 
   let { arc } = result
-  if (arc.tables && arc.tables.some(t=> Object.keys(t)[0] === 'data'))
+  if (arc.tables && arc.tables.some(t => Object.keys(t)[0] === 'data'))
     return `${arc.app[0]}-staging-data`
   throw ReferenceError('@tables not defined! begin/data expects a data table')
 }
