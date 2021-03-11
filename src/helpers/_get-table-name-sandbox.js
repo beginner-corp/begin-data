0dec295729014d9fe0d0adf3745c995972c58106
kj <kristoferjoseph@gmail.com>
2021-03-10 17:59:28 -0800

    Something like this, but still not finding the @tables even when sandbox does.

--- src/helpers/_get-table-name-sandbox.js
+++ src/helpers/_get-table-name-sandbox.js
@@ -1,35 +1,50 @@
-// let { join } = require('path')
-// FIXME should export read
+let { join } = require('path')
 let read = require('@architect/inventory/src/read')
-// let { readArc } = require('@architect/parser')
 
 /**
  * Arc 6+ paths where project manifest needs to be read from root
  */
 module.exports = function getTableName () {
-  let config = process.env.__ARC_CONFIG__
-  let cwd = process.env.NODE_ENV === 'testing'
-    ? config && JSON.parse(config).projectSrc
-    : process.cwd()
   let type = 'projectManifest'
   let err = ReferenceError('@tables not defined! begin/data expects a data table')
 
   try {
-    // attempt to read the arcfile
-    let { arc } = read({ type, cwd })
-
-    // look for a table named 'data'
-    if (arc.tables && arc.tables.some(t => Object.keys(t)[0] === 'data')) {
-      // return the local table
-      return `${arc.app[0]}-staging-data`
+    let config = process.env.__ARC_CONFIG__
+    let sandboxRoot = JSON.parse(config).projectSrc
+    let lastDitch = join(process.cwd(), '..', '..', '..')
+    if (getLocalDataTable(type, process.cwd())) {
+      return getLocalDataTable(type, process.cwd())
+    }
+    else if (getLocalDataTable(type, sandboxRoot)) {
+      return getLocalDataTable(type, sandboxRoot)
+    }
+    else if (getLocalDataTable(type, lastDitch)) {
+      return getLocalDataTable(type, lastDitch)
     }
     else {
       throw err
     }
   }
   catch (e) {
-    console.error(e)
     throw err
+  }
+
+  function getLocalDataTable (type, cwd) {
+    // attempt to read the arcfile
+    let { arc } = read({ type, cwd })
+    console.log('============================')
+    console.log('ARC:::: \n', arc)
+    console.log('============================')
+    return hasDataTable(arc)
+      // return the local table
+      ? `${arc.app[0]}-staging-data`
+      : false
+  }
+
+  function hasDataTable (arc) {
+    // look for a table named 'data'
+    return arc.tables &&
+      arc.tables.some(t => Object.keys(t)[0] === 'data')
   }
 
   /*
