9410296f95692ac5eae828ef1b97f1bce35e0ed6
kj <kristoferjoseph@gmail.com>
2021-03-11 17:00:49 -0800

    Adds waterfall check for project manifest.

--- src/helpers/_get-table-name-sandbox.js
+++ src/helpers/_get-table-name-sandbox.js
@@ -1,60 +1,50 @@
-let { join } = require('path')
-let read = require('@architect/inventory/src/read')
+const path = require('path')
+const read = require('@architect/inventory/src/read')
 
 /**
  * Arc 6+ paths where project manifest needs to be read from root
  */
 module.exports = function getTableName () {
-  let type = 'projectManifest'
   let err = ReferenceError('@tables not defined! begin/data expects a data table')
 
   try {
-    let config = process.env.__ARC_CONFIG__
-    let sandboxRoot = JSON.parse(config).projectSrc
-    let lastDitch = join(process.cwd(), '..', '..', '..')
-    if (getLocalDataTable(type, process.cwd())) {
-      return getLocalDataTable(type, process.cwd())
+    let processRoot = getStagingDataTable({ cwd: process.cwd() })
+    let defaultLevel = getStagingDataTable({ cwd: path.resolve(path.join('..', '..', '..')) })
+    let firstLevel = getStagingDataTable({ cwd: path.resolve(path.join('..')) })
+    let secondLevel = getStagingDataTable({ cwd: path.resolve(path.join('..', '..')) })
+
+    if (processRoot) {
+      return processRoot
     }
-    else if (getLocalDataTable(type, sandboxRoot)) {
-      return getLocalDataTable(type, sandboxRoot)
+    else if (defaultLevel) {
+      return defaultLevel
     }
-    else if (getLocalDataTable(type, lastDitch)) {
-      return getLocalDataTable(type, lastDitch)
+    else if (firstLevel) {
+      return firstLevel
     }
-    else {
-      throw err
+    else if (secondLevel) {
+      return secondLevel
     }
+    else throw err
   }
   catch (e) {
+    console.error(e)
     throw err
   }
 
-  function getLocalDataTable (type, cwd) {
+  function getStagingDataTable ({ cwd }) {
     // attempt to read the arcfile
-    let { arc } = read({ type, cwd })
-    return hasDataTable(arc)
-      // return the local table
-      ? `${arc.app[0]}-staging-data`
-      : false
+    let { arc } = read({ type: 'projectManifest', cwd })
+    if (arc.tables) {
+      if (arc.tables.some(t => Object.keys(t)[0] === 'data')) {
+        return `${arc.app[0]}-staging-data`
+      }
+      else {
+        return false
+      }
+    }
+    else {
+      return false
+    }
   }
-
-  function hasDataTable (arc) {
-    // look for a table named 'data'
-    return arc.tables &&
-      arc.tables.some(t => Object.keys(t)[0] === 'data')
-  }
-
-  /*
-  // Try cwd first (usually testing)
-  let result = readArc()
-  // Try project root next (usually from within a Sandbox Lambda)
-  if (!result.filepath) result = readArc({ cwd: join(process.cwd(), '..', '..', '..') })
-  // If we still didn't find it, blow up
-  if (!result.filepath) throw ReferenceError('Architect project manifest not found')
-
-  let { arc } = result
-  if (arc.tables && arc.tables.some(t => Object.keys(t)[0] === 'data'))
-    return `${arc.app[0]}-staging-data`
-  throw ReferenceError('@tables not defined! begin/data expects a data table')
-  */
 }
